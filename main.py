84182ebbb725506c421875d94f37bec8ce93295e
lehnasamia <105779901+lehnasamia@users.noreply.github.com>
2022-11-12 16:52:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ with st.sidebar:
 
 
 if st.button(label="Détecter le sujet d'insatisfaction"):
-    prediction(modelEntraine, vectorizer, nbr_topic, tex
+    prediction(modelEntraine, vectorizer, nbr_topic, text)

1f87a0cc7ce864cf72629a2a22f664e47e2615f5
lehnasamia <105779901+lehnasamia@users.noreply.github.com>
2022-11-12 17:17:46 +0100

    Update prediction_fonction.py

--- prediction_fonction.py
+++ prediction_fonction.py
@@ -87,8 +87,8 @@ def preprocess_text(text):
 
 '''Importer les fichiers pickles 'model' et 'vectoriseur'''
 
-with (open ("modelEntraineLehna",'rb')) as model_pickle:
-    modelEntraine = load(model_pickle)
+model_pickle = open ("modelEntraineLehna",'rb')    
+modelEntraine = load(model_pickle)
 
 
 vectoriseur_pickle = open ("vectoriseurLehna",'rb')
